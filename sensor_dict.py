4de50e7b1720efb96c63bb32a11a834882f6f01c
Gokul Vemulapalli <gokulv0331@gmail.com>
2026-05-11 12:56:03 +0200

    Added real-time timestamps and permanent file logging

--- sensor_dict.py
+++ sensor_dict.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 pressure_sensor = {
     "id": "P-201",
     "reading": 120,
@@ -13,5 +15,11 @@ flow_sensor = {
 factory_sensors = [pressure_sensor, flow_sensor]
 
 for sensor in factory_sensors:
-    print(f"Sensor ID {sensor['id']} is reading {sensor['reading']} {sensor['unit']}.")
+    timestamp = datetime.now()
+    clean_time = timestamp.strftime("%H:%M:%S")
+    print(f" {clean_time} - Sensor ID {sensor['id']} is reading {sensor['reading']} {sensor['unit']}.")
+    with open("sensor_log.txt", "a") as log_file:
+        log_file.write(f"{clean_time} - Sensor ID {sensor['id']} is reading {sensor['reading']} {sensor['unit']}.\n")
+
+
 
